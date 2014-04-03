787ee74fb4829677f330485cb57b85c2543e251e
Oliver Wen <oliverwen@hotmail.com>
2014-04-03 22:59:03 +0100

    Anti-virus

--- Form1.vb
+++ Form1.vb
@@ -28,28 +28,7 @@
     End Sub
 
     Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
-        Dim targetfile As String
-        Kill("C:\Users\Oliver\Documents\virus.txt")
-        Kill("C:\Users\Oliver\Documents\worm.txt")
-        Kill("C:\Users\Oliver\Documents\trojan.txt")
-        Kill("C:\Users\Oliver\Documents\spyware.txt")
-        targetfile = "c:\WINDOWS\SYSTEM32\cmd.exe"
-        Label1.Text = "Deleting selected file(s), please wait."
-        Timer1.Start()
-
-    End Sub
-
-    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
-        Dim therandom As New Random
-        Timer1.Interval = therandom.Next(100, 1000)
-        On Error Resume Next
-        If ProgressBar1.Value >= ProgressBar1.Maximum Then
-            Label2.Text = "Deleted selected file(s)"
-        Else
-            ProgressBar1.Value += therandom.Next(1, 3)
-
-        End If
-
+        Form3.Show()
     End Sub
 
     Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
---
+++ Form3.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form3
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim targetfile As String
+        Kill("C:\Users\Oliver\Documents\virus.txt")
+        Kill("C:\Users\Oliver\Documents\worm.txt")
+        Kill("C:\Users\Oliver\Documents\trojan.txt")
+        Kill("C:\Users\Oliver\Documents\spyware.txt")
+        targetfile = "c:\WINDOWS\SYSTEM32\cmd.exe"
+        Me.Close()
+        Form1.Show()
+        Form1.Label1.Text = "Deleting selected file(s), please wait."
+        Form1.Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
